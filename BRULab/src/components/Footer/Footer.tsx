73b5649ff575df5bf72cbeb74781f5a46cc1be54
IlyaKhmialkov <gromokotgrom@gmail.com>
2024-10-09 10:58:38 +0300

    feat: add contacts

--- BRULab/src/components/Footer/Footer.tsx
+++ BRULab/src/components/Footer/Footer.tsx
@@ -4,9 +4,15 @@ export function Footer() {
     return (
         <footer>
             <ul className={styles.contacts}>
-                <li>phone</li>
-                <li>email</li>
-                <li>other</li>
+                <li>
+                    <a href="tel:+1234567890">phone</a>
+                </li>
+                <li>
+                    <a href="mailto: aaa@gmail.com">email</a>
+                </li>
+                <li>
+                    <a href="#">other</a>
+                </li>
             </ul>
         </footer>
     );
